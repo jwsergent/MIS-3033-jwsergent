547ae5cf2b1ff929b2311884b94b1f8c42802b07
jwsergent <jwsergent@gmail.com>
2020-08-31 12:06:51 -0500

    Class work from 8/31/20

--- InClassExamples/InClassExamples/Program.cs
+++ InClassExamples/InClassExamples/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClassesFilesDateTime;
+using System;
+using System.Collections.Generic;
 
 namespace InClassExamples
 {
@@ -6,7 +8,24 @@ namespace InClassExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rect1 = new Rectangle();
+            rect1.height = 10;
+            rect1.width = 20;
+
+            Rectangle rect2 = new Rectangle(5, 2);
+            
+            Console.WriteLine($"The area of rect1 is {rect1.area()}");
+            Console.WriteLine($"The area of rect2 is {rect2.area()}");
+
+            List<Rectangle> rectangles = new List<Rectangle>();
+            rectangles.Add(rect1);
+            rectangles.Add(rect2);
+            rectangles.Add(new Rectangle(7, 7));
+
+            foreach (var rect in rectangles)
+            {
+                Console.WriteLine(rect);
+            }
         }
     }
 }
---
+++ InClassExamples/InClassExamples/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesFilesDateTime
+{
+    public class Rectangle
+    {
+        public int width { get; set; }
+        
+        public int height { get; set; }
+        public DateTime creationDate { get; set; }
+
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+            creationDate = DateTime.Now;
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double area()
+        {
+            return width * height;
+        }
+
+        public override string ToString()
+        {
+            string output=  $"The rectangle has a width of {width} and a height of {height}. The area of the rectangle is {area()}";
+
+            return output;
+        }
+    }
+}
