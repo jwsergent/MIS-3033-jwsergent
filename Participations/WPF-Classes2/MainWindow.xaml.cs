3b39bf0fe3432497e40ba7fbe42a3ebbcd9460ac
jwsergent <jwsergent@gmail.com>
2020-09-25 16:06:33 -0500

    Finished WPF 2 Classes Participation

---
+++ Participations/WPF-Classes2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Classes2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Toy NewToy = new Toy();
+            
+            
+            
+           
+            NewToy.Price = Convert.ToDouble(txtPrice.Text);
+
+            if (string.IsNullOrEmpty(txtManufacturer.Text))
+            {
+                MessageBox.Show("Please enter a manufacturer");
+            }
+            else
+            {
+                NewToy.Manufacturer = txtManufacturer.Text;
+            }
+            
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Please enter a toy name");
+            }
+            else
+            {
+                NewToy.Name = txtName.Text;
+            }
+
+            
+
+            
+
+            if (double.TryParse(txtPrice.Text.ToString(), out double f))
+            {
+                NewToy.Price = Convert.ToDouble(txtPrice.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid price");
+            }
+
+            lbToys.Items.Add(NewToy);
+        }
+
+        private void lbToys_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Toy selectedToy = (Toy)lbToys.SelectedItem;
+            MessageBox.Show(selectedToy.GetAisle());
+        }
+    }
+}
---
+++ Participations/WPF-Classes2/Toy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Classes2
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Aisle = "";
+        }
+
+        char ManuFirstLetter;
+        string UpperManu;
+        public string GetAisle()
+        {
+            UpperManu = Manufacturer.ToUpper();
+
+            ManuFirstLetter = UpperManu[0];
+
+            double NewPrice = Price * 100;
+            string PriceAisle = NewPrice.ToString("n0");
+
+
+            string Aisle = ManuFirstLetter + PriceAisle;
+            
+
+            return Aisle;
+        }
+
+        public override string ToString()
+        {
+            string Output = Manufacturer + " - " + Name;
+
+            return Output;
+        }
+    }
+}
