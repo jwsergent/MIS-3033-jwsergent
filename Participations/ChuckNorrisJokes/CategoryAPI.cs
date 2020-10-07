25b95283bc601dc586b0ade0062545b13af57676
jwsergent <jwsergent@gmail.com>
2020-10-07 16:03:43 -0500

    Partially finished chuck norris

---
+++ Participations/ChuckNorrisJokes/CategoryAPI.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokes
+{
+    class CategoryAPI
+    {
+        
+    }
+
+    public class Joke
+    {
+        
+
+        public string value { get; set; }
+
+        public override string ToString()
+        {
+            return value;
+        }
+
+    }
+
+    
+
+    
+}
---
+++ Participations/ChuckNorrisJokes/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChuckNorrisJokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+            string[] cats;
+
+            using (var client = new HttpClient())
+            {
+                string jsonResults = client.GetStringAsync(@"https://api.chucknorris.io/jokes/categories").Result;
+
+                cats = JsonConvert.DeserializeObject<string[]>(jsonResults);
+            }
+
+            cboCategories.Items.Add("All");
+
+            foreach (var item in cats)
+            {
+                cboCategories.Items.Add(item);
+            }
+
+            
+        }
+
+        private void cboCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedcategory = cboCategories.SelectedItem;
+
+            if (selectedcategory=="All")
+            {
+                string jokeurl = "https://api.chucknorris.io/jokes/random";
+
+                Joke RandomJoke;
+
+                using (var client = new HttpClient())
+                {
+                    string jsonres = client.GetStringAsync(jokeurl).Result;
+
+                    RandomJoke = JsonConvert.DeserializeObject<Joke>(jsonres);
+                }
+
+
+                txtJoke.Text = RandomJoke.ToString();
+
+            }
+            else
+            {
+                string jokeurl = "https://api.chucknorris.io/jokes/random?category=" + "{" + selectedcategory + "}";
+
+                Joke NewJoke;
+
+                using (var client = new HttpClient())
+                {
+                    string json = client.GetStringAsync(jokeurl).Result;
+
+                    NewJoke = JsonConvert.DeserializeObject<Joke>(json);
+                }
+
+                txtJoke.Text = NewJoke.ToString();
+            }
+            
+        }
+
+        
+    }
+}
